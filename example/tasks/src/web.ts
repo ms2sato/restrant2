a67f235246a511d81e3f5e5e14f4a87c4f4c35b8
Masashi Sato <ms2.sato@gmail.com>
2022-04-22 14:18:13 +0900

    add public dir

--- example/tasks/src/web.ts
+++ example/tasks/src/web.ts
@@ -50,6 +50,7 @@ export async function setup() {
   app.use(express.urlencoded({ extended: false }))
   app.set('views', path.join(__dirname, '../views'))
   app.set('view engine', 'pug')
+  app.use(express.static(path.join(__dirname, '../public')))
 
   app.use(
     fileUpload({
