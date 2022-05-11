dc2a3f0e6011c37e693a75fbb6e0a95bd4263d50
Masashi Sato <ms2.sato@gmail.com>
2022-05-11 10:33:52 +0900

    add NullActionContext

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export * from './lib/router'
 export * from './lib/server-router'
+export * from './lib/null-action-context'
 export * from './lib/parse-form-body'
 export * from './lib/create-zod-traverse-arranger-creator'
 export * from './lib/zod-util'
